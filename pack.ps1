f6a54fca9b4ab0e4a9b5b0d832a6d83149d82ce6
HanByul Yang <hanbyul.yang@gmail.com>
2016-09-27 16:13:19 +0900

    added nuget package generation script

---
+++ pack.ps1
@@ -0,0 +1,12 @@
+$root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
+$version = [System.Reflection.Assembly]::LoadFile("$root\CSIntelPerfLibs\CSIntel.Ipp\bin\Release\CSIntel.Ipp.dll").GetName().Version
+$versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
+
+Write-Host "Setting .nuspec version tag to $versionStr"
+
+$content = (Get-Content $root\CSIntelPerfLibs.nuspec) 
+$content = $content -replace '\$version\$',$versionStr
+
+$content | Out-File $root\CSIntelPerfLibs.compiled.nuspec
+
+& nuget pack $root\CSIntelPerfLibs.compiled.nuspec
